f8927aafab312967e52008853c79ad0a913cefec
kanodap <hestia0705@gmail.com>
2025-03-13 19:36:12 +0900

    [fix] 앱 로딩시 splash 안나오는 현상 해결, firebase_options.dart 추가

---
+++ sticky/lib/config/firebase_options.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+      apiKey: "AIzaSyA94vCO8JRVL2DVkMOC_UUuYen5tyjlbbU",
+      authDomain: "sticky-1b9ef.firebaseapp.com",
+      projectId: "sticky-1b9ef",
+      storageBucket: "sticky-1b9ef.appspot.com",
+      messagingSenderId: "11649697141",
+      appId: "1:11649697141:android:56aeeafc01677e0525ff3c",
+    );
+  }
+}
--- sticky/lib/main.dart
+++ sticky/lib/main.dart
@@ -5,33 +5,36 @@ import 'package:get/get.dart';
 import 'config/theme/my_theme.dart';
 import 'app/data/local/my_shared_pref.dart';
 import 'app/routes/app_pages.dart';
-
+import 'config/firebase_options.dart'; // ✅ Firebase 설정 추가
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  await MySharedPref.init();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform, // ✅ Firebase 초기화
+  );
+
+  await MySharedPref.init(); // ✅ SHARED PREFERENCE 초기화 추가
 
   runApp(
     ScreenUtilInit(
       designSize: const Size(375, 812),
       minTextAdapt: true,
       splitScreenMode: true,
-      rebuildFactor: (old, data) => true,
+      rebuildFactor: (old, data) => old != data, // ✅ 불필요한 리빌드 방지
       useInheritedMediaQuery: true,
-      builder: (context, widget) {
+      builder: (context, child) {
+        bool themeIsLight = MySharedPref.getThemeIsLight();
         return GetMaterialApp(
-       
           useInheritedMediaQuery: true,
-             title: "STICKY",
+          title: "STICKY",
           debugShowCheckedModeBanner: false,
-          builder: (context,widget) {
-            bool themeIsLight = MySharedPref.getThemeIsLight();
+          builder: (context, widget) {
             return Theme(
               data: MyTheme.getThemeData(isLight: themeIsLight),
               child: MediaQuery(
-                child: widget!,
                 data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+                child: widget ?? SizedBox(), // ✅ null 방지
               ),
             );
           },
